3bf482c6b36731177f1cb1937bd1ca0c9731228f
bromokun <bromokun@gmail.com>
2017-05-04 21:49:27 +0700

    update member test

--- test/models/member_test.exs
+++ test/models/member_test.exs
@@ -2,28 +2,26 @@ defmodule Portal.MemberTest do
   use Portal.ModelCase
 
   alias Portal.Member
-  alias Portal.Room
-  alias Portal.User
 
   @valid_attrs %{role: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
-    params = Map.put(@valid_attrs, :user, %{
-        name: "some name",
-        username: "username",
-        password: "password"
-    })
-    |> Map.put(:room, %{
-        name: "some room",
-        mode: "some mode"
-    })
-    changeset = Member.changeset(%Member{}, params)
-    assert changeset.valid?
+      params = Map.put(@valid_attrs, :user, %{
+          name: "some name",
+          username: "username",
+          password: "password"
+      })
+      |> Map.put(:room, %{
+          name: "some room",
+          mode: "some mode"
+      })
+      changeset = Member.changeset(%Member{}, params)
+      assert changeset.valid?
   end
 
   test "changeset with invalid attributes" do
-    changeset = Member.changeset(%Member{}, @invalid_attrs)
-    refute changeset.valid?
+      changeset = Member.changeset(%Member{}, @invalid_attrs)
+      refute changeset.valid?
   end
 end

43d363fa2a26010d07fc33f61359f536cf16ae17
bromokun <bromokun@gmail.com>
2017-05-05 17:52:20 +0700

    integrating vue.js

--- web/static/js/lobby.js
+++ web/static/js/lobby.js
@@ -1,3 +1,6 @@
+import Vue from 'vue'
+import PortalApp from "../components/portal_app.vue"
+
 let lobby = {
     init(socket, element) {
         if (!element) {
@@ -7,6 +10,17 @@ let lobby = {
             let username = element.getAttribute("data-username")
             let lobbyChannel = socket.channel("lobby:" + username)
             lobbyChannel.join()
+
+            // Create the main component
+            Vue.component('portal-app', PortalApp)
+
+            // And create the top-level view model:
+            new Vue({
+                el: '#portal-app-container',
+                render(createElement) {
+                    return createElement(PortalApp, {})
+                }
+            });
         }
     }
 }

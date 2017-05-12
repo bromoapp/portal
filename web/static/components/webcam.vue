256f2b6c7863bc5f41a2442bda32c0ed288b942f
bromokun <bromokun@gmail.com>
2017-05-13 02:10:25 +0700

    updates

--- web/static/components/webcam.vue
+++ web/static/components/webcam.vue
@@ -14,20 +14,20 @@ export default {}
 <style>
 #cam_video_mask {
     width: 240px;
-    height: 240px;
-    border-radius: 120px;
+    height: 180px;
+    border-radius: 15px;
+    position: absolute;
+    top: 25px;
+    left: 30px;
     -webkit-mask-image: -webkit-radial-gradient(circle, white 100%, black 100%);
 }
 #cam_video {
-    width: 480px;
-    height: 480px;
-    position: absolute;
-    top: -120px;
-    left: -120px;
+    width: 240px;
+    height: 180px;
 }
 #cam_canvas {
-    width: 480px;
-    height: 480px;
+    width: 240px;
+    height: 180px;
     display: none;
 }
 </style>

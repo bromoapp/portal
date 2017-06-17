4bfc2d917dcd84fe41fe6ca0929e75cbb3d6ed0b
Bromo <bromoapptester@gmail.com>
2017-06-18 02:12:33 +0700

    updates

--- web/static/components/settings.vue
+++ web/static/components/settings.vue
@@ -6,7 +6,7 @@
     
                     <div class="modal-header">
                         <slot name="header">
-                            default header
+                            Settings
                         </slot>
                     </div>
     

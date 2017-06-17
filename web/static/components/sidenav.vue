6ae9e3ab259f3600d6a9ee284374593062c8a8d7
Bromo <bromoapptester@gmail.com>
2017-06-17 17:22:17 +0700

    updates

--- web/static/components/sidenav.vue
+++ web/static/components/sidenav.vue
@@ -101,6 +101,7 @@ export default {
     background-color: #111;
     overflow-x: hidden;
     padding-top: 0px;
+    transition: 0.5s;
 }
 
 .sidenav-cover {
@@ -113,6 +114,7 @@ export default {
     background-color: #111;
     overflow-x: hidden;
     padding-top: 0px;
+    transition: 0.5s;
 }
 
 /* Position and style the close button (top right corner) */

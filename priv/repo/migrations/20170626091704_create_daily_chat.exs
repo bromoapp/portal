4f1d56bebb97ab1bec59492dd5b9b32bcba5dc82
Bromo <bromoapptester@gmail.com>
2017-07-02 22:34:49 +0700

    updates

--- priv/repo/migrations/20170626091704_create_daily_chat.exs
+++ priv/repo/migrations/20170626091704_create_daily_chat.exs
@@ -1,14 +1,26 @@
 defmodule Portal.Repo.Migrations.CreateDailyChat do
   use Ecto.Migration
 
-  def change do
-    create table(:daily_chats) do
-      add :read, :boolean, default: true
-      add :messages, :string
-      add :user_a_id, references(:users, on_delete: :nothing)
-      add :user_b_id, references(:users, on_delete: :nothing)
-
-      timestamps()
-    end
+  def up do
+    execute "
+      CREATE TABLE `daily_chats` (
+        `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `read` TINYINT(1) NULL DEFAULT '1',
+        `messages` LONGTEXT NULL,
+        `user_a_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+        `user_b_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+        `inserted_at` DATETIME NOT NULL,
+        `updated_at` DATETIME NOT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE INDEX `id` (`id`),
+        INDEX `daily_chats_user_a_id_fkey` (`user_a_id`),
+        INDEX `daily_chats_user_b_id_fkey` (`user_b_id`),
+        CONSTRAINT `daily_chats_user_a_id_fkey` FOREIGN KEY (`user_a_id`) REFERENCES `users` (`id`),
+        CONSTRAINT `daily_chats_user_b_id_fkey` FOREIGN KEY (`user_b_id`) REFERENCES `users` (`id`)
+      )
+      COLLATE='utf8_general_ci'
+      ENGINE=InnoDB
+      ;"
   end
+  
 end

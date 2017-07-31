aad3f935819191c866f318caec7886fe8bcb48aa
Bromo <bromoapptester@gmail.com>
2017-07-31 19:36:09 +0700

    updates

--- web/models/group.ex
+++ web/models/group.ex
@@ -1,18 +1,20 @@
 defmodule Portal.Group do
     use Portal.Web, :model
 
+    @default_status "PASSIVE"
+
     schema "groups" do
         field :name, :string
         field :members, :string
         field :admins, :string
+        field :status, :string, default: @default_status
 
         timestamps()
     end
 
     def create_or_update_changeset(struct, params) do
         struct
-        |> cast(params, [:name, :members, :admins])
-        |> validate_required([:name, :members, :admins])
-        |> foreign_key_constraint(:creator)
+        |> cast(params, [:name, :members, :admins, :status])
+        |> validate_required([:name, :admins])
     end
 end

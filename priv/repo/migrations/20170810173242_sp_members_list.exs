cf088b9d844f406dda5872a0b5de74ead522752e
Bromo <bromoapptester@gmail.com>
2017-08-11 00:41:15 +0700

    updates

---
+++ priv/repo/migrations/20170810173242_sp_members_list.exs
@@ -0,0 +1,9 @@
+defmodule Portal.Repo.Migrations.SpMembersList do
+  use Ecto.Migration
+
+  def up do
+    execute "
+      
+    "
+  end
+end
--- web/channels/user_group.ex
+++ web/channels/user_group.ex
@@ -4,9 +4,13 @@ defmodule Portal.UserGroup do
     alias Portal.Updates
     require Logger
 
+    # Event topics
     @p2g_msg_out "p2g_msg_out"
     @p2g_msg_new "p2g_msg_new"
     @p2g_msg_in "p2g_msg_in"
+
+    # SQLs
+    @sql_members_list "CALL `sp_members_list`(?);"
 
     #=================================================================================================
     # Functions related to members connections and presences
@@ -14,7 +18,7 @@ defmodule Portal.UserGroup do
     def join("user_group:" <> unique, _params, socket) do
         send self(), :after_join
 
-        {_user, updates} = {socket.assigns.user, %Updates{}}
+        {_socket, updates} = {socket, %Updates{}}
         |> _get_members_list()
         |> _get_ongoing_chats()
         {:ok, updates, socket}
@@ -32,11 +36,18 @@ defmodule Portal.UserGroup do
         {:noreply, socket}
     end
 
-    defp _get_members_list({user, struct}) do
-        {user, %Updates{struct | members: []}}
+    defp _get_members_list({socket, struct}) do
+        unique = _parse_unique(socket.topic)
+        user = socket.assigns.user
+        %Result{rows: rows} = SQL.query!(Repo, @sql_members_list, [unique, user.id])
+        if rows == [] do
+            {user, %Updates{struct | members: []}}
+        else
+            {user, %Updates{struct | members: []}}
+        end
     end
 
-    defp _get_ongoing_chats({user, struct}) do
+    defp _get_ongoing_chats({socket, struct}) do
         {user, %Updates{struct | chats: []}}
     end
 

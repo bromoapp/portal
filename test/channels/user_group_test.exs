df2e9a3a469ea517e763711bcfd18e879df2d80e
Bromo <bromoapptester@gmail.com>
2017-09-05 15:40:05 +0700

    updates

--- test/channels/user_group_test.exs
+++ test/channels/user_group_test.exs
@@ -7,15 +7,18 @@ defmodule Portal.UserGroupTest do
         
     @user_a %{name: "my_name_a", username: "my_username_a", password: "my_password"}
     @user_b %{name: "my_name_b", username: "my_username_b", password: "my_password"}
-
+    @user_c %{name: "my_name_c", username: "my_username_c", password: "my_password_c"}
+    
     setup do
         # create users
         user_a = insert_user(@user_a)
         user_b = insert_user(@user_b)
-
+        user_c = insert_user(@user_c)
+        
         # create relation
         insert_new_relation(user_a, user_b)
-
+        insert_new_relation(user_a, user_c)
+        
         # create group
         insert_new_group()
     end
